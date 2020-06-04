cac97ab9e68ef5a0a460e318cecbfe17a1224a3c
TK Soh <teekaysoh@gmail.com>
2020-06-04 11:32:49 +0800

    gspreaddb: add more support for module testing

--- src/gspreaddb.py
+++ src/gspreaddb.py
@@ -37,26 +37,62 @@ class GspreadDB:
     def log(self, time, msg):
         self.logsheet.append_row([time, str(msg)])
 
+    def getdatarow(self, stockname, row):
+        sheet = self.getstocksheet(stockname)
+        return sheet.row_values(row+1)
+
+    def getcolnames(self, stockname):
+        sheet = self.getstocksheet(stockname)
+        return sheet.row_values(1)
+
 if __name__ == "__main__":
     from datetime import datetime
-    now = datetime.now()
-    nowtime = now.strftime("%d/%m/%Y %H:%M:%S")
-        
-    db = GspreadDB()
-    
-    # write to a valid sample stock sheet for testing
-    db.add("TESTING", nowtime, 1.234)
-    
-    # write to unlisted stocks 
+    import getopt
+    import sys
+    import pprint
+
+    argv = sys.argv[1:]
+    writetodb = False
+    jsonfile = 'iopv.json'
     try:
-        db.add("XXX", nowtime, 1.234)
-    except ValueError as ve:
-        db.log(nowtime, ve)
-        print(ve)
+        # parse command line options
+        opts, args = getopt.getopt(argv, 'wj:')
+        Options = dict(opts)
+        if '-w' in Options.keys():
+            writetodb = True
+        if '-j' in Options.keys():
+            jsonfile = Options['-j']
+    except getopt.GetoptError:
+        #Print a message or do something useful
+        print('Invalid command line option or arguments')
+        sys.exit(2)
+
+    # access the database
+    db = GspreadDB(json=jsonfile)
+
+    # make sure we can read the database
+    colnames = db.getcolnames("TESTING")
+    rowdata = db.getdatarow("TESTING", 1)
+    pprint.pprint([colnames, rowdata])
+
+    # test write to database
+    if writetodb:
+        now = datetime.now()
+        nowtime = now.strftime("%d/%m/%Y %H:%M:%S")
         
-    # write to missing stocks sheet 
-    try:
-        db.add("ABF Malaysia Bond Index Fund", nowtime, 1.234)
-    except ValueError as ve:
-        db.log(nowtime, ve)
-        print(ve)
+        # write to a valid sample stock sheet for testing
+        db.add("TESTING", nowtime, 1.234)
+
+        # write to unlisted stocks
+        try:
+            db.add("XXX", nowtime, 1.234)
+        except ValueError as ve:
+            db.log(nowtime, ve)
+            print(ve)
+
+        # write to missing stocks sheet
+        try:
+            db.add("ABF Malaysia Bond Index Fund", nowtime, 1.234)
+        except ValueError as ve:
+            db.log(nowtime, ve)
+            print(ve)
