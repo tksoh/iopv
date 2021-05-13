bc272bddc58b85b669d2c1451adc61ff01916150
TK Soh <teekaysoh@gmail.com>
2021-05-13 13:34:55 +0800

    stockcharts: don't use secondary y axis for close line

--- src/stockcharts.py
+++ src/stockcharts.py
@@ -378,7 +378,7 @@ def make_chart(df, stock):
     )
     fig.add_trace(
         go.Scatter(x=df.DATE, y=df['CLOSE'], name="CLOSE", visible='legendonly',
-                   line={'color': "darkcyan"}), secondary_y=True,
+                   line={'color': "darkcyan"}),
         row=1, col=1
     )
     fig.add_trace(
